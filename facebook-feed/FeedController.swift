4f8515081a3deb470c5544a72ef517cf0f30eabb
nikhil <nikhil.nangia85@gmail.com>
2018-08-28 16:17:10 +0530

    Added ui image view and textview  for status

--- facebook-feed/FeedController.swift
+++ facebook-feed/FeedController.swift
@@ -38,7 +38,7 @@ class FeedController: UICollectionViewController {
 extension FeedController: UICollectionViewDelegateFlowLayout{
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         
-        return CGSize(width: view.frame.width, height: 60)
+        return CGSize(width: view.frame.width, height: 300)
     }
 }
 
