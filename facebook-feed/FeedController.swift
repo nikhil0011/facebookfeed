8cea40ea6534ac14b6aad551e946056e13a3ac2e
nikhil <nikhil.nangia85@gmail.com>
2018-08-29 23:46:00 +0530

    all the fields are being set dynamically by ui collection view data set : "post: Post?"

--- facebook-feed/FeedController.swift
+++ facebook-feed/FeedController.swift
@@ -11,15 +11,31 @@ let cellid = "cellid"
 
 class FeedController: UICollectionViewController {
     var posts = [Post]()
+//    var flowLayout: UICollectionViewFlowLayout {
+//        return self.collectionView?.collectionViewLayout as! UICollectionViewFlowLayout
+//    }
     override func viewDidLoad() {
         super.viewDidLoad()
+//        self.flowLayout.estimatedItemSize = CGSize(width: view.frame.width, height: 344)
         let postbyMark = Post()
         postbyMark.name = "Mark Zuckerberg"
-        postbyMark.statusText = "You code only once but it runs by millions"
-
+        postbyMark.statusText = "You code only once but it effects by millions Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi porta sodales neque, quis porta elit faucibus quis. Morbi vitae luctus nibh, id volutpat mauris. Integer sagittis sagittis nisi vitae "
+        postbyMark.profileImageName = "zuckprofile"
+        postbyMark.location = "January 18 ・ San Hose ・"
+        postbyMark.statusImageName = "gandhi_status"
+        postbyMark.numLikes = 190
+        postbyMark.numComment = 100000000
+        
         let postbySteve = Post()
         postbySteve.name = "Alan Walker"
         postbySteve.statusText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi porta sodales neque, quis porta elit faucibus quis. Morbi vitae luctus nibh, id volutpat mauris. Integer sagittis sagittis nisi vitae suscipit. Phasellus nisl mauris, gravida ac neque a, venenatis pulvinar eros. Sed tempus tellus quam, ut convallis orci lobortis sit amet. Donec enim risus, dignissim ac velit non, varius iaculis turpis. Nam et elementum leo. Aliquam vehicula mi quis justo dictum euismod. Praesent sit amet porttitor nisl, sit amet semper neque. Donec sit amet ipsum eget mi aliquet iaculis eget et ligula. Etiam a orci non libero vestibulum fermentum nec quis mauris."
+        postbySteve.profileImageName = "steve_profile"
+        postbySteve.location = "December 18 ・ London ・"
+        postbySteve.statusImageName = "steve_status"
+        postbySteve.numLikes = 440
+        postbySteve.numComment = 9900
+        
+        
         posts.append(postbyMark)
         posts.append(postbySteve)
         
@@ -47,12 +63,26 @@ class FeedController: UICollectionViewController {
 }
 
 extension FeedController: UICollectionViewDelegateFlowLayout{
+    
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
         if let statusText = posts[indexPath.item].statusText{
             let rect = NSString(string: statusText).boundingRect(with: CGSize(width: view.frame.width, height: 1000), options: NSStringDrawingOptions.usesDeviceMetrics.union(NSStringDrawingOptions.usesLineFragmentOrigin),attributes: [NSAttributedStringKey.font: UIFont.systemFont(ofSize: 14)], context: nil)
             return CGSize(width: view.frame.width, height: rect.height + 370.0)
         }
         return CGSize(width: view.frame.width, height: 370)
+//        return UICollectionViewFlowLayoutAutomaticSize
     }
 }
-
+extension UIView {
+    var parentViewController: UIViewController? {
+        var parentResponder: UIResponder? = self
+        while parentResponder != nil {
+            parentResponder = parentResponder!.next
+            if let viewController = parentResponder as? UIViewController {
+                return viewController
+            }
+        }
+        return nil
+    }
+}
